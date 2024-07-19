447b4f87c821e781c8add573a2d62482a3d78e4d
Karolina Sebestova <karolina.sebestova@nxp.com>
2024-07-19 15:54:03 +0200

    Add copyright and license header to source files

--- unity_config/suite_tear_down.c
+++ unity_config/suite_tear_down.c
@@ -1,12 +1,18 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <zephyr/sys/printk.h>
 
 /*
-* This function provides Twister (Zephyr's test runner script) integration.
-*
-* All it does is print out certain strings to the console depending on whether
-* any of the tests failed. Twister can then parse these strings which lets it
-* know if a test suite has passed or failed.
-*/
+ * This function provides Twister (Zephyr's test runner script) integration.
+ *
+ * All it does is print out certain strings to the console depending on whether
+ * any of the tests failed. Twister can then parse these strings which lets it
+ * know if a test suite has passed or failed.
+ */
 int suiteTearDown(int num_failures) {
     if (num_failures == 0) {
         printk("UNITY PROJECT EXECUTION SUCCESSFUL\r\n");
--- unity_config/unity_config.h
+++ unity_config/unity_config.h
@@ -1,2 +1,8 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #define UNITY_END() suiteTearDown(UnityEnd())
 #define UNITY_OUTPUT_COLOR
