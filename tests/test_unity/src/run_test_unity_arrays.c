1f6aee2ac6bfcdee80d8631b33b9febea3052aba
Karolina Sebestova <karolina.sebestova@nxp.com>
2024-07-23 16:43:17 +0200

    Wrap included files in definition macro

--- tests/test_unity/src/run_test_unity_arrays.c
+++ tests/test_unity/src/run_test_unity_arrays.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifdef TEST_UNITY_ARRAYS
+
 #include "run_test_unity.h"
 #include "test_unity_arrays.c"
 // #include "test_unity_core.c"
@@ -22,6 +24,8 @@ void run_test_unity_arrays(void) {
     RUN_TEST(testInt64ArrayWithinDeltaAndMessage);
     RUN_TEST(testInt64ArrayNotWithinDelta);
 }
+
+#endif
 
 // void run_test_unity_doubles(void) {
 //     RUN_TEST(testDoublesWithinDelta);
--- tests/test_unity/src/run_test_unity_core.c
+++ tests/test_unity/src/run_test_unity_core.c
@@ -1,6 +1,10 @@
+#ifdef TEST_UNITY_CORE
+
 #include "run_test_unity.h"
 #include "test_unity_core.c"
 
 void run_test_unity_core(void) {
     RUN_TEST(testUnitySizeInitializationReminder);
 }
+
+#endif
