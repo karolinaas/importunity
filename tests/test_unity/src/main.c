b5b83504fb5001b297af522840e48e68a8f78c20
Karolina Sebestova <karolina.sebestova@nxp.com>
2024-07-23 09:59:02 +0200

    Add self tests for Unity

---
+++ tests/test_unity/src/main.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "unity.h"
+#include "run_test.h"
+
+// void setUp(void) {
+
+// }
+
+// void tearDown(void) {
+
+// }
+
+int main(void) {
+    UNITY_BEGIN();
+    run_test_unity_arrays();
+    return UNITY_END();
+}
