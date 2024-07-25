ccc6699b02108addffacb1ffe633b6c7ae9e9417
Karolina Sebestova <karolina.sebestova@nxp.com>
2024-07-25 11:00:52 +0200

    Expand documentation

--- unity_config/unity_config.h
+++ unity_config/unity_config.h
@@ -4,5 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/*
+ * Custom UNITY_END macro is needed to retain vanilla Unity usage while also
+ * providing Twister integration
+ */
 #define UNITY_END() suiteTearDown(UnityEnd())
+
 #define UNITY_OUTPUT_COLOR
